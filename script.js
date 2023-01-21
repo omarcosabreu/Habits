f0ee37540e92bac07a1546732fe8efd3b025810c
Marcos Abreu <marcosmendes.abreu7@gmail.com>
2023-01-20 22:16:59 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add) 
+form.addEventListener('change', save)
+
+  function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+    
+    if (dayExists) {
+      alert("Dia já registrado!")
+      return
+    }
+
+    console.log('Adicionado com sucesso ✅')
+    nlwSetup.addDay(today)
+  }
+
+  function save() {
+    localStorage.setItem("ToolsKey", JSON.stringify(nlwSetup.data))
+  }
+  
+ const data = JSON.parse(localStorage.getItem("ToolsKey")) || {}
+ nlwSetup.setData(data)
+ nlwSetup.load()
+
+
+
