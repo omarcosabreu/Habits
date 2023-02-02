d00a71ff3e6cc3088bf4b47612c49f110ebb4fd2
Marcos Abreu <marcosmendes.abreu7@gmail.com>
2023-02-02 00:15:29 -0300

    update var name

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
+var name = prompt("Olá!! Qual o seu nome?")
+
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
-button.addEventListener('click', add) 
-form.addEventListener('change', save)
+button.addEventListener("click", add)
+form.addEventListener("change", save)
 
-  function add() {
-    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
-    const dayExists = nlwSetup.dayExists(today)
-    
-    if (dayExists) {
-      alert("✋🏽Dia já registrado!")
-      return
-    }
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
 
-    console.log('Adicionado com sucesso ✅')
-    nlwSetup.addDay(today)
+  if (dayExists) {
+    alert( 'Hey ' + name + ', esse dia já foi registrado!✋🏽')
+    return
   }
 
-  function save() {
-    localStorage.setItem("ToolsKey", JSON.stringify(nlwSetup.data))
-  }
-  
- const data = JSON.parse(localStorage.getItem("ToolsKey")) || {}
- nlwSetup.setData(data)
- nlwSetup.load()
+  console.log("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
 
+function save() {
+  localStorage.setItem("ToolsKey", JSON.stringify(nlwSetup.data))
+}
 
-
+const data = JSON.parse(localStorage.getItem("ToolsKey")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
